ecdb84410a1354883b13b8701df5bc8d55dcd412
Ali Alhashimi <libmacali@gmail.com>
2024-08-24 08:39:19 +0400

    update server port

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8900;
 
 async function startServer() {
     await loadData();
